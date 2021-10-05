881da40d78754f6caad1a306aeacd2f7d87d4c1d
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-10-05 16:16:11 +0300

    Initial commit

---
+++ src/Kata.java
@@ -0,0 +1,14 @@
+
+
+    public class Kata {
+
+        public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+            double length;
+            double width;
+            double height;
+            return 0;
+        }
+
+    }
+
+
